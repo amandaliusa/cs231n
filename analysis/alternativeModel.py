d7e3a643ece75a6028fff2aabf77d8fa2ce10679
mandyleung <ms.mandy@gmail.com>
2023-06-08 06:34:42 +0000

    Added 3Layer, RNN and LSTM models

--- analysis/alternativeModel.py
+++ analysis/alternativeModel.py
@@ -24,21 +24,33 @@ class ThreeLayer_model(nn.Module):
         return scores
     
 class RNN_model(nn.Module):
-    def __init__(self, input_size, hidden_size1, hidden_size2, num_classes):
+    def __init__(self, input_size, rnn_hidden_size, lin_hidden_size, num_classes, num_frames):
         super().__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size1)
-        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
-        self.fc3 = nn.Linear(hidden_size2, num_classes)
+        self.rnn = nn.RNN(input_size=input_size, hidden_size=rnn_hidden_size, batch_first=True)
+        self.fc1 = nn.Linear(rnn_hidden_size, lin_hidden_size)
+        self.fc2 = nn.Linear(lin_hidden_size, num_classes)
+        self.num_frames = num_frames
     
     def forward(self, x):
-        
-        scores = self.fc3(F.relu(self.fc2(F.relu(self.fc1(x)))))
+        N = x.shape[0]
+        x = x.view((N, self.num_frames, -1))
+        _, h_rnn = self.rnn(x)       # (num_layers * num_directions, batch, hidden_size)
+        h_lin = self.fc1(F.relu(h_rnn.squeeze(dim=0)))
+        scores = self.fc2(F.relu(h_lin))
         return scores
     
-# Initialize the RNN.
-rnn = nn.RNN(input_size=INPUT_SIZE, hidden_size=HIDDEN_SIZE, num_layers = 1, batch_first=True)
-# input size : (batch, seq_len, input_size)
-inputs = data.view(BATCH_SIZE, SEQ_LENGTH, INPUT_SIZE)
-# out shape = (batch, seq_len, num_directions * hidden_size)
-# h_n shape  = (num_layers * num_directions, batch, hidden_size)
-out, h_n = rnn(inputs)
+class LSTM_model(nn.Module):
+    def __init__(self, input_size, lstm_hidden_size, lin_hidden_size, num_classes, num_frames, dropout):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=lstm_hidden_size, batch_first=True, dropout=dropout)
+        self.fc1 = nn.Linear(lstm_hidden_size, lin_hidden_size)
+        self.fc2 = nn.Linear(lin_hidden_size, num_classes)
+        self.num_frames = num_frames
+    
+    def forward(self, x):
+        N = x.shape[0]
+        x = x.view((N, self.num_frames, -1))
+        _, (h_n, _) = self.lstm(x)       # (num_layers * num_directions, batch, hidden_size)
+        h_lin = self.fc1(F.relu(h_n.squeeze(dim=0)))
+        scores = self.fc2(F.relu(h_lin))
+        return scores
